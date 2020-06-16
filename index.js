109b8ceacf40e8235034df40ab6892985b8c1b99
lamecksilva <lameck2@hotmail.com>
2020-06-16 07:21:41 -0300

    FEAT: Discord Hello World

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client } = require('discord.js');
+
+const client = new Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN || 'token');

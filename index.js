8aebe6565711088a72dbef199c3b80abccf72e21
lamecksilva <lameck2@hotmail.com>
2020-06-16 08:31:55 -0300

    FEAT: Conectado ao canal de voz

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const { Client } = require('discord.js');
 
 const client = new Client();
 
+const db = [];
+
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Login efetuado com sucesso ${client.user.tag}!`);
 });
 
-client.on('message', (msg) => {
-	if (msg.content === 'ping') {
-		msg.reply('pong');
+client.on('message', (message) => {
+	if (message.content === 'toda vez que eu vejo voce') {
+		// message.reply('pong');
+		message.channel.send('sinto uma coisa diferente');
+	}
+
+	if (message.content.startsWith('!ponto')) {
+		const conectado = message.member.voice?.channel?.name.startsWith('work');
+
+		if (conectado) {
+			message.reply(
+				'Conectado ao canal de trabalho, iniciando jornada de trabalho.'
+			);
+		} else {
+			message.reply('Você precisa estar conectado a algum canal de trabalho.');
+		}
 	}
 });
 
